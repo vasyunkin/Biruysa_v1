0b6aa69b46a92e364b39072b49d6af424e0d86e2
vasyunkin <vasunkon1@gmail.com>
2026-06-26 12:23:48 +0700

    недоделанный шаблон проекта

---
+++ src/main/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+templates = Jinja2Templates(directory="src/presentation/templates")
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="Biryusa",
+        docs_url="/docs",
+        redoc_url="/redoc"
+    )
+
+    app.mount("/static", StaticFiles(directory="src/presentation/static"), name="static")
+
+    @app.get("/", response_class=HTMLResponse)
+    async def home(request: Request):
+        template = templates.env.get_template("index.html")
+        content = template.render({"request": request})
+        return HTMLResponse(content)
+
+    return app
+
+app = create_app()
